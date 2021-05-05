f415cb1191333a3263431c1fc06fcef0a6540abf
webAqemu <notych2812@gmail.com>
2021-05-05 23:23:59 +0300

    added adaptive version to index html and fixed some bugs

--- js/script.js
+++ js/script.js
@@ -4,6 +4,29 @@ $(".doctors__inner").slick({
   slidesToScroll: 4,
   prevArrow: `<button type="button" class="doctors__arrow doctors__arrow--left"></button>`,
   nextArrow: `<button type="button" class="doctors__arrow doctors__arrow--right"></button>`,
+  responsive: [
+    {
+      breakpoint: 1150,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 870,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 // second slider on index.html
 $(".certificates__inner").slick({
@@ -12,6 +35,44 @@ $(".certificates__inner").slick({
   arrows: false,
   dots: true,
   dotsClass: "certificates__dots",
+  responsive: [
+    {
+      breakpoint: 1150,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 870,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 // delete numbers from slider dots
 document.querySelectorAll(".certificates__dots button").forEach((btn) => (btn.innerHTML = ""));
+
+// header burger
+const burger = document.querySelector(".burger");
+const headerNav = document.querySelector(".header__nav");
+burger.addEventListener("click", function () {
+  burger.classList.toggle("active");
+  headerNav.classList.toggle("active");
+});
+
+// toggle header__hover
+const headerList = document.querySelector(".menu__item--list");
+headerList.addEventListener("click", function () {
+  document.querySelector(".header__hover").classList.toggle("active");
+  document.querySelector(".header__hover-icon").classList.toggle("active");
+});
